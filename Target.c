b88c7d59b280c65f5694bf4972361e6c830190dd
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-10-21 21:38:11 +0530

    #include <stdio.h> int main() { 	int P1,P2,P3,P4; 	scanf("%d%d%d%d",&P1,&P2,&P3,&P4); 	if(P1>=10 && P2>=10 && P3>=10 && P4>=10) 	{ 		printf("YES"); 	} 	else 	{ 		printf("NO"); 	} }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int P1,P2,P3,P4;
+	scanf("%d%d%d%d",&P1,&P2,&P3,&P4);
+	if(P1>=10 && P2>=10 && P3>=10 && P4>=10)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+}
