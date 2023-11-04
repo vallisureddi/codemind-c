57e1a8ee66c12037484323688d27d511f953c987
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-11-04 19:40:07 +0530

    #include <stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);         if(n%3==0)         {             printf("Pling");         }         if(n%5==0)         {             printf("Plang");         }         if(n%7==0)         {             printf("Plong");         }         if(n%3!=0&&n%5!=0&&n%7!=0)         printf("%d",n); }

---
+++ RainDrop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+        if(n%3==0)
+        {
+            printf("Pling");
+        }
+        if(n%5==0)
+        {
+            printf("Plang");
+        }
+        if(n%7==0)
+        {
+            printf("Plong");
+        }
+        if(n%3!=0&&n%5!=0&&n%7!=0)
+        printf("%d",n);
+}
