ce26316f11f8b4fcc2c353d7b5069938fc8f0ee7
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-18 11:16:53 +0530

    #include <stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     for(int i=a;i<=b;i++)     {         if(i%2==0 && i%2==0)             {                 printf("%d ",i);             }     } }

---
+++ Print_even_numbers_up_to_a_given_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    for(int i=a;i<=b;i++)
+    {
+        if(i%2==0 && i%2==0)
+            {
+                printf("%d ",i);
+            }
+    }
+}
