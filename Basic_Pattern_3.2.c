bd0686bb0121d2c9ad481f88b9b4d06cf8c9fa4c
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-18 10:51:51 +0530

    #include <stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         for(j=a;j>=i;j--)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_3.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        for(j=a;j>=i;j--)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
