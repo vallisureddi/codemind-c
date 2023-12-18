4ac1bb80965f3bd806723faad452f53e73217716
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-18 09:52:46 +0530

    #include <stdio.h> int main() {     int n,r,rev=0;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     if(n<0)     {         printf("-%d",rev);     }     else     {         printf("%d",rev);     } }

---
+++ Reverse_Integer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,r,rev=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if(n<0)
+    {
+        printf("-%d",rev);
+    }
+    else
+    {
+        printf("%d",rev);
+    }
+}
