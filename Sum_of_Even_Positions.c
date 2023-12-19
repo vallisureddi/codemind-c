f0612aa50887474e219d65f38d0ab48fbec4985e
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-19 10:56:40 +0530

    #include <stdio.h> int main() {     int  a,i,sum=0;     scanf("%d",&a);     int arr[a];     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<a;i++){     if(i%2==0)     {         sum=sum+arr[i];     }     }     printf("%d",sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int  a,i,sum=0;
+    scanf("%d",&a);
+    int arr[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<a;i++){
+    if(i%2==0)
+    {
+        sum=sum+arr[i];
+    }
+    }
+    printf("%d",sum);
+}
