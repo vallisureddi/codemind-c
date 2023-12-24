ae348c0e6697fa432fa030f520f54e51581dbeae
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-24 11:20:01 +0530

    #include <stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=0;i--)     {         if(arr[i]%2==1)         break;     }         printf("%d",arr[i]); }

---
+++ Last_odd_number_in_an_Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(arr[i]%2==1)
+        break;
+    }
+        printf("%d",arr[i]);
+}
