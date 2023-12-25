cc6b50d596ac46cd746f13000c6bb5a127cb868a
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2023-12-25 12:32:40 +0530

    #include <stdio.h> int main() {     int a,b,c=0;     scanf("%d%d",&a,&b);     for(int i=a+1;i<b;i++)     {         c=0;         for(int j=1;j<=i;j++)         {             if(i%j==0)         {             c++;         }         }         if(c==2)         printf("%d ",i);     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c=0;
+    scanf("%d%d",&a,&b);
+    for(int i=a+1;i<b;i++)
+    {
+        c=0;
+        for(int j=1;j<=i;j++)
+        {
+            if(i%j==0)
+        {
+            c++;
+        }
+        }
+        if(c==2)
+        printf("%d
+",i);
+    }
+}
