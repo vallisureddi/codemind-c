1f927674f5e166bcc6561fb0cf07375eb5a6fe5f
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2024-01-20 10:30:48 +0530

    #include <stdio.h> int main() {     int a,i,j,k;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(k=i;k<a;k++)         {             printf(" ");         }         for(j=i;j>=1;j--)         {             printf("%d ",j);         }         printf(" ");     } }

---
+++ Pattern_7.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,j,k;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(k=i;k<a;k++)
+        {
+            printf(" ");
+        }
+        for(j=i;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("
+");
+    }
+}
