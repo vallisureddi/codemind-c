5c04d9706ca8f69a51cb080e52397b94dda09abb
vallisureddi <147476019+vallisureddi@users.noreply.github.com>
2024-01-20 10:14:58 +0530

    #include <stdio.h> int main() {     int a,i,j;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=1;j<=a;j++)         {             if(i==j||i+j==a+1)             {                 printf("%d",i);             }             else             printf(" ");         }         printf(" ");     } }

---
+++ Pattern_8.3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int a,i,j;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=1;j<=a;j++)
+        {
+            if(i==j||i+j==a+1)
+            {
+                printf("%d",i);
+            }
+            else
+            printf(" ");
+        }
+        printf("
+");
+    }
+}
